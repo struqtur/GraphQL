04a9b06f920603609c98b4db068320864890ea58
Matěj Votruba <fsfmatej@gmail.com>
2023-11-21 17:33:05 +0100

    Add fallback for missing scalar types

--- src/Type/TypeFactory.php
+++ src/Type/TypeFactory.php
@@ -32,8 +32,13 @@ class TypeFactory
 
                 $className = 'Youshido\GraphQL\Type\Scalar\\' . $name . 'Type';
 
+                // Hotfix for our custom app - TODO - add via configuration
                 if ($name == 'DateTimeAsString' || $name == 'StringOrArray' || $name == 'Boolean' || $name == 'Float' || $name == 'Int') {
                     $className = 'App\GraphQL\Schema\Type\Scalar\\' . $name . 'Type';
+                    // Fallback to base scalar types if custom doesn't exist
+                    if (!file_exists(dirname($className . '.php'))) {
+                        $className = 'Youshido\GraphQL\Type\Scalar\\' . $name . 'Type';
+                    }
                 }
 
                 self::$objectsHash[$type] = new $className();
